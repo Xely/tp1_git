fe5bbc11fe5d986aff4e4897a5cb7af552142735
Xely <julien_guya@msn.com>
2017-01-10 12:16:36 +0100

    first two classes

---
+++ src/Jockey.java
@@ -0,0 +1,7 @@
+public class Jockey {
+
+    public String nom;
+    public String prenom;
+    public int age;
+
+}
---
+++ src/Poney.java
@@ -0,0 +1,8 @@
+public class Poney {
+
+    public String nom;
+    public int id;
+    public int age;
+    public Jockey jockey;
+
+}
